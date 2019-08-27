e9724751aaa898e8a5963eb0c87feb267347d637
pinchyCZN <pinchyCZN@gmail.com>
2019-08-27 16:06:31 -0500

    adding more gui files

--- gui.c
+++ gui.c
@@ -1,2 +1,40 @@
 #include <Windows.h>
+#include "resource.h"
+HINSTANCE g_hinstance=0;
 
+BOOL CALLBACK tab_func(HWND hwnd,UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	return FALSE;
+}
+
+BOOL CALLBACK dlg_func(HWND hwnd,UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	switch(msg){
+	case WM_INITDIALOG:
+		{
+			
+			HWND hdlg,hparent=GetDlgItem(hwnd,IDC_TAB_SHEET);
+			hdlg=CreateDialog(g_hinstance,MAKEINTRESOURCE(IDD_SETTINGS),hparent,tab_func);
+			ShowWindow(hdlg,SW_SHOW);
+		}
+		break;
+	case WM_COMMAND:
+		{
+			int id = LOWORD(wparam);
+			switch(id){
+			case IDOK:
+				break;
+			case IDCANCEL:
+				EndDialog(hwnd,0);
+				break;
+			}
+		}
+		break;
+	}
+	return FALSE;
+}
+int APIENTRY WinMain(HINSTANCE hinst,HINSTANCE hprev,LPSTR cmd_line,int cmd_show)
+{
+	g_hinstance=hinst;
+	DialogBox(hinst,MAKEINTRESOURCE(IDD_MAIN_DLG),NULL,dlg_func);
+}
---
+++ resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by resource.rc
+//
+#define IDD_MAIN_DLG                    101
+#define IDD_SETTINGS                    103
+#define IDD_LOG_GATEWAY                 104
+#define IDC_TAB_SHEET                   1001
+#define IDC_BUTTON1                     1002
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
