e03a732eabaa18d04ac8536f47447beb11ff2bcd
pinchyCZN <pinchyCZN@gmail.com>
2019-09-03 16:19:28 -0500

    updates

--- gui.c
+++ gui.c
@@ -2,15 +2,25 @@
 #include <CommCtrl.h>
 #include <stdio.h>
 #include "resource.h"
+#include "anchor_system.h"
+#pragma warning(disable:4996)
+
 HINSTANCE g_hinstance=0;
 
 typedef struct{
-	int index;
 	HWND hwnd;
+	HWND hparent;
 	char text[80];
 }TAB_DATA;
 
 TAB_DATA tab_data[8]={0};
+
+static struct CONTROL_ANCHOR anchor_settings[]={
+	{IDC_USER_NAME,ANCHOR_LEFT|ANCHOR_RIGHT|ANCHOR_TOP,0,0,0},
+	{IDC_PASSWORD,ANCHOR_LEFT|ANCHOR_RIGHT|ANCHOR_TOP,0,0,0},
+	{IDC_SHOW_PASSWORD,ANCHOR_RIGHT|ANCHOR_TOP,0,0,0},
+	{IDC_SAVE_SETTINGS,ANCHOR_RIGHT|ANCHOR_BOTTOM,0,0,0},
+};
 
 int print_lasterror()
 {
@@ -22,9 +32,32 @@ int print_lasterror()
 	return 0;
 }
 
+int get_dlg_parent(const HWND hwnd,HWND *hparent)
+{
+	int i,count;
+	count=_countof(tab_data);
+	for(i=0;i<count;i++){
+		TAB_DATA *tmp=&tab_data[i];
+		if(hwnd==tmp->hwnd){
+			hparent[0]=tmp->hparent;
+			return TRUE;
+		}
+	}
+	return FALSE;
+}
 
 BOOL CALLBACK settings_func(HWND hwnd,UINT msg, WPARAM wparam, LPARAM lparam)
 {
+	switch(msg){
+	case WM_INITDIALOG:
+		{
+			HWND hparent=0;
+			get_dlg_parent(hwnd,&hparent);
+			if(hparent)
+				AnchorInit(hparent,anchor_settings,_countof(anchor_settings));
+		}
+		break;
+	}
 	return FALSE;
 }
 
@@ -35,14 +68,21 @@ int add_tab_page(HWND htab,int idd,DLGPROC dlg_proc,char *text)
 	int count;
 	TC_ITEM item={0};
 	count=TabCtrl_GetItemCount(htab);
-	hdlg=CreateDialog(g_hinstance,MAKEINTRESOURCE(idd),htab,dlg_proc);
-	if(0==hdlg)
-		return result;
-	SetWindowPos(hdlg,NULL,20,20,100,100,SWP_NOZORDER|SWP_NOSIZE|SWP_SHOWWINDOW);
-	//ShowWindow(hdlg,SW_SHOWNORMAL);
-	item.mask=TCIF_TEXT|TCIF_PARAM;
-	item.pszText=text;
-	result=TabCtrl_InsertItem(htab,count,&item);
+	if(count<_countof(tab_data)){
+		TAB_DATA *tmp=&tab_data[count];
+		tmp->hparent=htab; 
+		strncpy(tmp->text,text,sizeof(tmp->text));
+		tmp->text[sizeof(tmp->text)-1]=0;
+		hdlg=CreateDialog(g_hinstance,MAKEINTRESOURCE(idd),htab,dlg_proc);
+		if(0==hdlg)
+			return result;
+		tmp->hwnd=hdlg;
+		SetWindowPos(hdlg,NULL,20,20,100,100,SWP_NOZORDER|SWP_NOSIZE|SWP_SHOWWINDOW);
+		//ShowWindow(hdlg,SW_SHOWNORMAL);
+		item.mask=TCIF_TEXT|TCIF_PARAM;
+		item.pszText=text;
+		result=TabCtrl_InsertItem(htab,count,&item);
+	}
 	return result;
 }
 
--- resource.h
+++ resource.h
@@ -9,10 +9,12 @@
 #define IDD_LOG_IRC                     106
 #define IDC_TAB_SHEET                   1001
 #define IDC_BUTTON1                     1002
-#define IDC_BUTTON2                     1003
-#define IDC_BUTTON3                     1004
-#define IDC_BUTTON4                     1005
-#define IDC_TAB1                        1006
+#define IDC_SAVE_SETTINGS               1002
+#define IDC_ENABLE_DISCORD              1007
+#define IDC_USER_NAME                   1008
+#define IDC_PASSWORD                    1009
+#define IDC_CHECK2                      1010
+#define IDC_SHOW_PASSWORD               1010
 
 // Next default values for new objects
 // 
@@ -20,7 +22,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_CONTROL_VALUE         1011
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
