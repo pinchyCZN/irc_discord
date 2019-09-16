d887a68bac8cd9600b0db74a65f57b52e9b1f094
pinchyCZN <pinchyCZN@gmail.com>
2019-09-16 16:05:40 -0500

    updates

--- config.c
+++ config.c
@@ -1,5 +1,6 @@
 #include <Windows.h>
 #include <stdio.h>
+#include "libstring.h"
 
 #pragma warning(disable:4996)
 
@@ -159,13 +160,115 @@ static int set_ini_value(const WCHAR *section,const WCHAR *key,const WCHAR *str)
 	return result;
 }
 
+static int rand_seed=0x1337;
+
+static int xor_str(BYTE *data,int data_len)
+{
+	int i;
+	srand(rand_seed);
+	for(i=0;i<data_len;i++){
+		BYTE a=data[i];
+		a^=rand();
+		data[i]=a;
+	}
+	return TRUE;
+}
+
+//allocates string
+static char *data_to_hex(BYTE *data,int data_len)
+{
+	int i,len;
+	char *tmp;
+	len=data_len*2;
+	tmp=calloc(len+1,1);
+	if(tmp){
+		for(i=0;i<len;i++){
+			int index=i>>1;
+			BYTE a=data[index];
+			if(i&1){
+				a=a&0xF;
+			}else{
+				a=a>>4;
+				a&=0xF;
+			}
+			if(a>=10){
+				a='A'+(a-10);
+			}else{
+				a='0'+a;
+			}
+			tmp[i]=a;
+		}
+	}
+	return tmp;
+}
+
+static int hex_to_data(char *hex,BYTE **out,int *out_len)
+{
+	int result=FALSE;
+	int len;
+	BYTE *tmp;
+	int tmp_size;
+	int i,count,index;
+	BYTE val;
+	len=strlen(hex);
+	tmp_size=len/2;
+	tmp=calloc(tmp_size,1);
+	if(0==tmp){
+		return result;
+	}
+	index=count=0;
+	val=0;
+	for(i=0;i<len;i++){
+		BYTE a=hex[i];
+		int x=0;
+		count++;
+		if(a>='A'){
+			a&=~0x20;
+			x=a-'A'+10;
+		}else{
+			x=a-'0';
+		}
+		val<<=4;
+		val|=x&0xF;
+		if(count>=2){
+			tmp[index]=val;
+			count=0;
+			index++;
+		}
+	}
+	*out=tmp;
+	*out_len=index;
+	result=TRUE;
+	return result;
+}
+
 int save_user_name(const WCHAR *user)
 {
 	return set_ini_value(L"SETTINGS",L"USERNAME",user);
 }
 int save_password(const WCHAR *password)
 {
-	return set_ini_value(L"SETTINGS",L"PASSWORD",password);
+	int result=FALSE;
+	int len;
+	char *tmp;
+	len=wcslen(password)*sizeof(WCHAR);
+	tmp=malloc(len);
+	if(tmp){
+		char *hex;
+		memcpy(tmp,password,len);
+		xor_str(tmp,len);
+		hex=data_to_hex(tmp,len);
+		if(hex){
+			WCHAR *wstr=utf2wchar(hex);
+			if(wstr){
+				result=set_ini_value(L"SETTINGS",L"PASSWORD",wstr);
+				free(wstr);
+			}
+			free(hex);
+		}
+		free(tmp);
+	}
+	return result;
 }
 
 const char *get_user_name()
@@ -178,9 +281,32 @@ const char *get_user_name()
 
 const char *get_password()
 {
-	static char password[255]={0};
+	static char password[512];
+	char tmp[512]={0};
+	char *data=0;
+	int data_len=0;
 	memset(password,0,sizeof(password));
-	get_ini_value(L"SETTINGS",L"PASSWORD",password,sizeof(password));
+	get_ini_value(L"SETTINGS",L"PASSWORD",tmp,sizeof(tmp));
+	hex_to_data(tmp,&data,&data_len);
+	xor_str(data,data_len);
+	if(data_len>sizeof(tmp)){
+		data_len=sizeof(tmp);
+	}
+	if(data){
+		char *str;
+		int end=sizeof(tmp);
+		memset(tmp,0,sizeof(tmp));
+		memcpy(tmp,data,data_len);
+		tmp[end-1]=0;
+		tmp[end-2]=0;
+		str=wchar2utf(tmp);
+		if(str){
+			strncpy(password,str,sizeof(password));
+			password[sizeof(password)-1]=0;
+			free(str);
+		}
+		free(data);
+	}
 	return password;
 }
 
